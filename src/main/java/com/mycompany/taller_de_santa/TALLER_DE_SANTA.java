a1a7adcccb3f2008bcf20eb8d5721434716b716f
Walter Ruiz <walter_ruiz29@hotmail.es>
2025-11-17 21:11:06 -0600

    limpiando el main

--- src/main/java/com/mycompany/taller_de_santa/TALLER_DE_SANTA.java
+++ src/main/java/com/mycompany/taller_de_santa/TALLER_DE_SANTA.java
@@ -6,25 +6,9 @@ import java.util.Scanner;
 
 public class TALLER_DE_SANTA {
 
-    private static ArrayList <Usuario> usuarios = new ArrayList<>();
-    private static Scanner SC = new Scanner(System.in);
-    
+  
     public static void main(String[] args) {
     
-        System.out.println("REGISTRO DE USUARIOS");
-        
-        while(true){
-        System.out.println("Por favor ingrese su nombre de usuario: ");
-        String NombreUsuario= Scanner.nextLine();
-        
-        if(existeusuario(NombreUsuario)){
-        System.out.println("El Usuario Ingresado ya existe.");
-        continue;
-        
-        
-        
-          }
-        
-        }
+
     }
 }
